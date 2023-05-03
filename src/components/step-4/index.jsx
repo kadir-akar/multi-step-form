083485f0fcbe1bf275c2a7cfe32aa145dd6ae54e
Kadir Akar <aakarkadir@gmail.com>
2023-05-03 19:29:22 +0300

    bug fixed

--- src/components/step-4/index.jsx
+++ src/components/step-4/index.jsx
@@ -3,19 +3,40 @@ import Step from "../step";
 
 import * as S from "./styled";
 
-const Step4 = ({ formData, ...props }) => {
+function Step4({ formData, ...props }) {
+  const { billingType, plan } = formData.step2;
+  const { selectedAddons } = formData.step3;
+
   return (
     <Step {...props}>
       <S.Step4>
         <S.MainRow>
           <S.Title>
-            {formData.step2} ({formData.step2.billingType})
+            {plan.title} ({billingType}){/* <button>change</button> */}
           </S.Title>
-          <S.Price>Price</S.Price>
+          <S.Price>{plan.price}</S.Price>
         </S.MainRow>
+        {selectedAddons.length > 0 &&
+          selectedAddons.map((item) => (
+            <S.SubRow key={item.id}>
+              <S.Title>{item.title}</S.Title>
+              <S.Price>{item.price}</S.Price>
+            </S.SubRow>
+          ))}
+        <S.TotalRow>
+          <S.Title>Total (per {billingType})</S.Title>
+          <S.TotalAmount>
+            +$
+            {selectedAddons.reduce(
+              (acc, curr) => acc + curr.priceAmount,
+              plan.priceAmount
+            )}
+            /{billingType === "monthly" ? "mo" : "yr"}
+          </S.TotalAmount>
+        </S.TotalRow>
       </S.Step4>
     </Step>
   );
-};
+}
 
 export default Step4;
